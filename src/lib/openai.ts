55113a9b0b0ab19da50efd680ba134b18b8a0d09
Roee Shapira <ro33.sha@gmail.com>
2023-06-12 15:16:27 +0300

    Chagne response to list of messages

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -1,11 +1,11 @@
-import { ChatCompletion, OpenAI } from "openai";
+import { OpenAI } from "openai";
 
 const OPENAI_API_KEY = Deno.env.get("OPENAI_API_KEY");
 if (!OPENAI_API_KEY) throw Error("No openai API key");
 
 const OPEN_AI = new OpenAI(OPENAI_API_KEY);
 
-export async function getCompletions(prDiff: string): Promise<ChatCompletion> {
+export async function getCompletions(prDiff: string) {
 	const chatCompletion = await OPEN_AI.createChatCompletion({
 		model: "gpt-3.5-turbo",
 		messages: [
@@ -18,5 +18,5 @@ export async function getCompletions(prDiff: string): Promise<ChatCompletion> {
 			},
 		],
 	});
-	return chatCompletion;
+	return chatCompletion.choices.map((c) => c.message.content);
 }
