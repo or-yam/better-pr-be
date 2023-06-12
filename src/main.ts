01a6482bd4b1e8972447620463b701780a70a756
Roee Shapira <ro33.sha@gmail.com>
2023-06-12 13:24:17 +0300

    Add method validation

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,20 @@ import { RequestBody } from "./lib/types.ts";
 
 export function main() {
 	return serve(async (request: Request) => {
-		const body = await request.json();
-		if (typeof body !== "object") throw Error("Failed to parse request body");
-		const { author, prNumber, repoName, repoUrl }: RequestBody = body;
-		console.log({ author, prNumber, repoName, repoUrl });
+		if (request.method === "POST") {
+			const body = await request.json();
+			if (typeof body !== "object") throw Error("Failed to parse request body");
+			const { author, prNumber, repoName, repoUrl }: RequestBody = body;
+			console.log({ author, prNumber, repoName, repoUrl });
 
-		const comp = await getCompletions();
-		const compStr = JSON.stringify(comp);
+			const comp = await getCompletions();
+			const compStr = JSON.stringify(comp);
 
-		return new Response(new Blob([compStr]), { status: 200 });
+			return new Response(new Blob([compStr]), { status: 200 });
+		} else {
+			return new Response("Unsupported method " + request.method, {
+				status: 404,
+			});
+		}
 	});
 }
